c94a222710e2a03e86db453d93e1dd1030faf712
hechenglong <kfhechenglong@126.com>
2023-04-06 15:12:34 +0800

    feat(ejs): ✨ ejs模版

--- express/index.js
+++ express/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json(req.headers)
+app.set('views', './templates')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, resp) => {
+  const list = [
+    {
+      id: 123,
+      name: 'zhangsan'
+    },
+    {
+      id: 456,
+      name: '李四'
+    }
+  ]
+  resp.render('index', {
+    title: 'ejs',
+    content: '首页 home',
+    users: list,
+    show: true
+  })
 })
 const port = 9001
 app.listen(port, () => {
