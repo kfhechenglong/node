3ab3e945315c3cef44d7b6f16a511f8f3aff8746
hechenglong <kfhechenglong@126.com>
2023-04-06 17:53:55 +0800

    feat(ejs): ✨ ejs留言模版

--- express/index.js
+++ express/index.js
@@ -1,27 +1,40 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 app.set('views', './templates')
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+const messages = []
 
 app.get('/', (req, resp) => {
-  const list = [
-    {
-      id: 123,
-      name: 'zhangsan'
-    },
-    {
-      id: 456,
-      name: '李四'
-    }
-  ]
   resp.render('index', {
-    title: 'ejs',
-    content: '首页 home',
-    users: list,
-    show: true
+    messages
   })
 })
+
+app
+  .route('/publish')
+  .get((req, resq) => {
+    resq.render('publish')
+  })
+  .post((req, resp) => {
+    const name = req.body.name
+    const content = req.body.content
+    console.log(req.body)
+    console.log(name)
+    console.log(content)
+    if (!name || !content) {
+      throw new Error('请填写完整的信息内容！')
+    }
+    const now = new Date().toLocaleString()
+    messages.push({
+      name,
+      content,
+      time: now
+    })
+    resp.redirect('/')
+  })
+
 const port = 9001
 app.listen(port, () => {
   console.log('listen port ' + port)
